c489126956a5fac1fcb8e1b482dec9884ce70a1d
Niem Nguyen and Pablo Vera <pablo@>
2016-05-25 09:34:26 -0700

    add sass gulp task / var and folder/files

--- gulpfile.js
+++ gulpfile.js
@@ -19,6 +19,9 @@ var lib = require('bower-files')({
 });
 var browserSync = require('browser-sync').create();
 var jshint = require ('gulp-jshint');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var del = require('del');
 
 gulp.task('jshint', function(){
   return gulp.src(['js/*.js'])
@@ -71,6 +74,24 @@ gulp.task('build', ['clean'], function(){
     gulp.start('jsBrowserify');
   }
   gulp.start('bower');
+  gulp.start('cssBuild');
+});
+
+gulp.task('bowerBuild', ['bower'], function(){
+  browserSync.reload();
+});
+
+gulp.task('htmlBuild', function() {
+  browserSync.reload();
+});
+
+gulp.task('cssBuild', function() {
+  return gulp.src('scss/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./build/css'))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('serve', function() {
@@ -84,13 +105,6 @@ gulp.task('serve', function() {
   gulp.watch(['js/*.js'], ['jsBuild']);
   gulp.watch(['bower.json'], ['bowerBuild']);
   gulp.watch(['*.html'], ['htmlBuild']);
+  gulp.watch(["scss/*.scss"], ['cssBuild']);
 
-});
-
-gulp.task('bowerBuild', ['bower'], function(){
-  browserSync.reload();
-});
-
-gulp.task('htmlBuild', function() {
-  browserSync.reload();
 });
