0e658d9b675a2b6ade0cb548ca0f652a79bf3724
Pablo Vera <pablovera.pdx@gmail.com>
2016-05-27 14:22:06 -0700

    Fix extra quote that was showing up

--- js/github.js
+++ js/github.js
@@ -7,6 +7,6 @@ exports.Github.prototype.getRepos = function(userName){
   $.get('https://api.github.com/users/' + userName + '?access_token=' + apiKey).then(function(response){
 
 
-    $("#result").append("<p>" + "<b>" + userName + "</b><br>" + "<img class='profilePhoto' src='" + response.avatar_url + "'>'");
+    $("#result").prepend("<b>" + userName + "</b><br>" + "<img class='profilePhoto' src=" + response.avatar_url + ">");
   });
 };
