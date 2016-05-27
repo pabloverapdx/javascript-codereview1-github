4f4ce494dc7d8da2d84ab763bc9c1d7bf0334b15
Pablo Vera <pablovera.pdx@gmail.com>
2016-05-27 11:59:39 -0700

    change .append to .text for now

--- js/github-interface.js
+++ js/github-interface.js
@@ -3,12 +3,13 @@ var Github = require ("./../js/github.js").Github;
 $(document).ready(function() {
 
 var userObject = new Github ();
+
+
+
   $("#userFind").click(function(event){
     event.preventDefault();
-
     var userName = $("#userSearch").val();
     $("#userSearch").val("");
-
     var user = userObject.getRepos(userName);
   });
 });
--- js/github.js
+++ js/github.js
@@ -5,6 +5,8 @@ exports.Github = function(){
 
 exports.Github.prototype.getRepos = function(userName){
   $.get('https://api.github.com/users/' + userName + '?access_token=' + apiKey).then(function(response){
-    $("#result").append("You searched for " + userName);
+
+
+    $("#result").text("You searched for " + userName);
   });
 };
