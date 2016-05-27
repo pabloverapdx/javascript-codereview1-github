548bb8f975ddad3e547b14eac3e10fc9310dbdd8
Pablo Vera <pablovera.pdx@gmail.com>
2016-05-27 11:41:06 -0700

    V1 of project. Add user interface and bussiness logic. So far input returns name of whatever it was entered. Also made a header and centered the tittle of the project inside the header using flex

--- js/github-interface.js
+++ js/github-interface.js
@@ -1,1 +1,14 @@
-var Github = require('./../js/github.js').Github;
+var Github = require ("./../js/github.js").Github;
+
+$(function(){
+
+var userObject = new Github ();
+  $("#userFind").click(function(event){
+    event.preventDefault();
+
+    var userName = $("#userSearch").val();
+    $("#userSearch").val("");
+
+    var user = userObject.getRepos(userName);
+  });
+});
--- js/github.js
+++ js/github.js
@@ -1,1 +1,10 @@
-var apiKey = require('./../.env').apiKey;
+var apiKey = require("./../.env").apiKey;
+
+exports.Github = function(){
+};
+
+exports.Github.prototype.getRepos = function(userName){
+  $.get('https://api.github.com/users/' + userName + '?access_token=' + apiKey).then(function(response){
+    $("#result").append("You searched for " + userName);
+  });
+};
