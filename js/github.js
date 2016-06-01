1073c41517be5d223104c73d2e07d451df921660
Pablo Vera <pablovera.pdx@gmail.com>
2016-06-01 07:37:25 -0700

    add comments to github.js

--- js/github.js
+++ js/github.js
@@ -3,10 +3,11 @@ var apiKey = require("./../.env").apiKey;
 exports.Github = function(){
 };
 
+ // API Call starter code //
 exports.Github.prototype.getRepos = function(userName){
   $.get('https://api.github.com/users/' + userName + '?access_token=' + apiKey).then(function(response){
 
-
+// Show results. Prepend user name + user photo //
     $("#result").prepend("<b>" + userName + "</b><br>" + "<img class='profilePhoto' src=" + response.avatar_url + ">");
   });
 };
